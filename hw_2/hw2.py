d4ec9e1a5579f19cc6a2b51c12485dbecbe26b2d
“Mister_Grin” <vanogrishin2007@gmail.com>
2024-02-29 16:54:35 +0100

    updated generating passwords

--- hw_2/hw2.py
+++ hw_2/hw2.py
@@ -11,14 +11,19 @@ def generate_password():
     password_length = random.randint(10, 20)
     password_symbols = string.ascii_letters + string.digits + string.punctuation
     list_password = []
-    for i in range(password_length):
-        password_choice = random.choice(password_symbols)
-        list_password.append(password_choice)
+    while True:
+        for i in range(password_length):
+            password_choice = random.choice(password_symbols)
+            list_password.append(password_choice)
+        if any(symbol.isdigit() for symbol in list_password):
+            break
+        else:
+            list_password.clear()
     password = ''.join(list_password)
     return f"<p>password: {password}</p>"
 
 
-@app.route("/calculating")
+@app.route("/students-average")
 def calculate_average():
     data_frame = pandas.read_csv('input_data/hw.csv')
     average_height = data_frame[' Height(Inches)'].mean()
